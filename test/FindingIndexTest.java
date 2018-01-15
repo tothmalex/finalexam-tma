a9d4726fa631ad9319884373b52a6fb04a978eac
Alex Toth <mihalyalextoth@gmail.com>
2018-01-15 10:02:44 +0100

    first exe done

---
+++ test/FindingIndexTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FindingIndexTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void main() {
+    }
+
+    @Test
+    public void indexHunter() {
+    }
+}
